f2037959af78e168bd38a376f9172baa5b359d53
Ade3164 <adebayoidris051@gmail.com>
2022-03-11 01:01:51 +0000

    nnnn

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-/**
- *  * main - A function that prints a string
- *   * Description: A function that prints a string
- *    * Return: Returns zero
- *     */
+/* idris */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
