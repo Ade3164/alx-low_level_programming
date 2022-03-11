5239f611536072be88f36dbc9fcf76ec188ad6bd
Ade3164 <adebayoidris051@gmail.com>
2022-03-11 01:08:36 +0000

    maain

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/* idris */
+/*  * Description: main - Prints out a string to stdout. Return: 0 if success.*    */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
