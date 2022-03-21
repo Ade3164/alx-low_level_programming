310e6dc4efb7d71dd80cbebd4e99475d7ccec146
Ade3164 <Adebayoidris051@gmail.com>
2022-03-21 11:15:42 +0100

    hAS

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *puts2 - print every other charachter of a string
+ *@str: string
+ *
+ *Return: nothing
+ */
+
+void puts2(char *str)
+{
+int i;
+i = 0;
+
+while (str[i])
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
