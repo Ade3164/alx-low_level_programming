b97b82d05906a32589f96e6004d855ad95ad53f7
Ade3164 <Adebayoidris051@gmail.com>
2022-04-01 22:55:27 +0100

    first

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+int main(int argc __attribute__((unused)), char **argv);
+int main(int argc, char **argv);
+int main(int argc, char **argv);
+int main(int argc, char **argv);
+int main(int argc, char **argv);
+int _putchar(char c);
