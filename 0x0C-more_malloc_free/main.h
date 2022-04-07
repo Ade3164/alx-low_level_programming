5faebce75c7d13f063fb04466c954b63859d3b6e
Ade3164 <Adebayoidris051@gmail.com>
2022-04-07 23:07:56 +0100

    mall

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,3 +4,7 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 int *array_range(int min, int max);
+char *_initialize_array(char *ar, int lar);
+void _is_zero(char *argv[]);
+int _checknum(char *argv[], int n);
+int main(int argc, char *argv[]);
