b117d3f320650ec084fecb7acb55e718c55d4e19
Ade3164 <adebayoidris051@gmail.com>
2022-04-13 20:37:45 +0100

    main

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - entry point to func
+*
+* Description: program that prints the opcodes of it own main function.
+* Return: Always 0
+*/
+int main(void)
+{
+	return (0);
+}
